ea38ced0be73f3948171dedc74062d652821e548
webdevtools4everyone <51172031+webdevtools4everyone@users.noreply.github.com>
2019-05-29 23:12:17 +0530

    Add files via upload

---
+++ My First webpage/sign_up.php
@@ -0,0 +1,10 @@
+
+<?php	$email=$_REQUEST['email'];
+        $uname=$_REQUEST['uname'];
+        $psw=$_REQUEST['psw'];
+        echo("Email :".$email);
+        echo("<br/>Password :".$psw);
+        echo("<br/>Username :".$uname);
+        echo("<br/>You are signed up");
+
+?>
